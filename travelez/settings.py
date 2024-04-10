b3b91c8d7ca461e03295dced6423258883b5d1d6
shh-its-me <155425155+shh-its-me@users.noreply.github.com>
2024-04-10 12:32:19 +0000

    new file:   adminapp/__pycache__/__init__.cpython-310.pyc 	new file:   adminapp/__pycache__/admin.cpython-310.pyc 	new file:   adminapp/__pycache__/apps.cpython-310.pyc 	new file:   adminapp/__pycache__/models.cpython-310.pyc 	new file:   adminapp/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   clientapp/__pycache__/__init__.cpython-310.pyc 	new file:   clientapp/__pycache__/admin.cpython-310.pyc 	new file:   clientapp/__pycache__/apps.cpython-310.pyc 	new file:   clientapp/__pycache__/models.cpython-310.pyc 	new file:   clientapp/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   db.sqlite3 	new file:   travelapp/__pycache__/__init__.cpython-310.pyc 	new file:   travelapp/__pycache__/admin.cpython-310.pyc 	new file:   travelapp/__pycache__/apps.cpython-310.pyc 	new file:   travelapp/__pycache__/models.cpython-310.pyc 	new file:   travelapp/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   travelez/__pycache__/__init__.cpython-310.pyc 	new file:   travelez/__pycache__/settings.cpython-310.pyc 	new file:   travelez/__pycache__/urls.cpython-310.pyc 	new file:   travelez/__pycache__/wsgi.cpython-310.pyc 	modified:   travelez/settings.py

--- travelez/settings.py
+++ travelez/settings.py
@@ -83,16 +83,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": 'travel',
-        "USER":'root',
-        "Password":'',
-        "HOST" :'localhost',
-        "PORT" :'3306'
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": 'travel',
+#         "USER":'root',
+#         "Password":'',
+#         "HOST" :'localhost',
+#         "PORT" :'3306'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
